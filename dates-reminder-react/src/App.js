115b24ebacce8d7bdf33297f7792e420a39993fa
cass <alicastilo1097@gmail.com>
2023-06-02 23:36:52 +0300

    CREATE

---
+++ dates-reminder-react/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap";
+
+
+function App() {
+  return (
+    <div className="">
+      <Button className="btn btn-danger">gt54t5t454t</Button>
+      </div>
+  );
+}
+
+export default App;
