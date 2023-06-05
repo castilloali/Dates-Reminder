bbd304b17fe167a4d04343de345ffbfd6e0b923a
cass <alicastilo1097@gmail.com>
2023-06-05 03:42:48 +0300

    CREATE all project

--- dates-reminder-react/src/App.js
+++ dates-reminder-react/src/App.js
@@ -1,10 +1,55 @@
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import "../src/index.css"
+import { person } from "./data";
 
 
 function App() {
   return (
     <div className="">
-      <Button className="btn btn-danger">gt54t5t454t</Button>
+      <Container className="container py-5" >
+        <Row className="justify-content-center">
+          <Col sm='8' className="fs-5 fw-bold mb-2">
+          لديك {person.length} مواعيد اليوم
+          </Col>
+        </Row>
+
+
+        <Row className="justify-content-center ">
+          <Col pill  sm='8'>
+            <div className="rectangle rounded px-4">
+
+
+
+            {
+              person.length ? person.map((item) =>{
+                return (
+                  <div className="d-flex align-content-center pt-3 border-bottom">
+                <img className="img-avatar" src={item.img} alt="dc" />
+                <div className="me-2 ">
+                  <h5 className="d-inline">{item.name}</h5>
+                  <p>{item.date}</p>
+                </div>
+              </div>
+                )
+              }) : <h2 className="d-flex justify-content-center pt-5 border-bottom">لا يوجد مواعيد اليوم</h2>
+            }
+
+
+
+
+
+            </div>
+          </Col>
+        </Row>
+
+
+        <Row className="justify-content-center mt-2">
+          <Col sm='8' className="d-flex justify-content-between">
+            <Button className="btn-cas ">مسح الكل</Button>
+            <Button className="btn-cas ">عرض البيانات</Button>
+          </Col>
+        </Row>
+      </Container>
       </div>
   );
 }
---
+++ dates-reminder-react/src/data.js
@@ -0,0 +1,7 @@
+export const person = [
+    {id : "1", name:"علي", date:"5 مساء", img:"../image/hhholographic.png"},
+    {id : "2", name:"محمد", date:"9 مساء", img:"../image/hhholographic.png"},
+    {id : "3", name:"حسين", date:"3 مساء", img:"../image/hhholographic.png"},
+    {id : "4", name:"مصطفي", date:"1 مساء", img:"../image/hhholographic.png"},
+    {id : "5", name:"سعد", date:"7 صباحا", img:"../image/hhholographic.png"}
+    ]
